43ce2a5554ef9496ddd24e58551f5a94b75866bb
Gyordan <jordancaminati@libero.it>
2022-05-04 15:04:55 +0200

    removed unused params and add pass callback

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -20,15 +20,15 @@ class TrialEvalCallback(EvalCallback):
     """
 
     def __init__(
-        self,
-        eval_env: VecEnv,
-        trial: optuna.Trial,
-        n_eval_episodes: int = 5,
-        eval_freq: int = 10000,
-        deterministic: bool = True,
-        verbose: int = 0,
-        best_model_save_path: Optional[str] = None,
-        log_path: Optional[str] = None,
+            self,
+            eval_env: VecEnv,
+            trial: optuna.Trial,
+            n_eval_episodes: int = 5,
+            eval_freq: int = 10000,
+            deterministic: bool = True,
+            verbose: int = 0,
+            best_model_save_path: Optional[str] = None,
+            log_path: Optional[str] = None,
     ):
 
         super().__init__(
@@ -227,7 +227,8 @@ class RawStatisticsCallback(BaseCallback):
                 self._tensorboard_writer.write(logger_dict, exclude_dict, self._timesteps_counter)
 
         return True
-        
+
+
 class Closer(BaseCallback):
     """
     Callback used for logging raw episode data (return and episode length).
@@ -241,16 +242,8 @@ class Closer(BaseCallback):
         pass
 
     def _on_step(self) -> bool:
-
         return True
-        
+
     def _on_training_end(self) -> None:
-        diff = ["easy", "medium", "hard"]
-        new_lvl = random.randint(0, 100)
-        model.get_env().change_obstacles(level=new_lvl, difficult=new_diff)  # )
-	
-        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-        pass 
-        
-        
-        
+        # self.training_env.kill_flightmare()
+        pass
