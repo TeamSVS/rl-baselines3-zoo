52e34c64c0cdbb77bae9461514ea33cc51b52c74
Gyordan <jordancaminati@libero.it>
2022-05-08 11:29:20 +0000

    Add big net_arch

--- utils/hyperparams_opt.py
+++ utils/hyperparams_opt.py
@@ -16,8 +16,8 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     :return:
     """
 
-    batch_size = trial.suggest_categorical("batch_size", [32, 32, 32, 32, 32])  # 32, 64, 128, 256, 512
-    n_steps = trial.suggest_categorical("n_steps", [20, 20, 20])  # 256, 512, 1024
+    batch_size = trial.suggest_categorical("batch_size", [32,64,128,256,512])  # 32, 64, 128, 256, 512
+    n_steps = trial.suggest_categorical("n_steps", [ 256,512,1024])  # 256, 512, 1024
     gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
     learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1)
     lr_schedule = "constant"
@@ -29,7 +29,7 @@ def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
     gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
     max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
     vf_coef = trial.suggest_uniform("vf_coef", 0, 1)
-    net_arch = trial.suggest_categorical("net_arch", ["small", "medium"])
+    net_arch = trial.suggest_categorical("net_arch", ["small","medium","big"])
     # Uncomment for gSDE (continuous actions)
     # log_std_init = trial.suggest_uniform("log_std_init", -4, 1)
     # Uncomment for gSDE (continuous action)
